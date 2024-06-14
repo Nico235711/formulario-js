17a1ec7f7fd94d5d3d04a62658d7bcdd071322c0
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-14 14:18:02 -0300

    terminando práctica de formulario

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,37 @@
 function recieveData(event) {
-  event.prevenDefault() // modifico la acción por defecto de un formulario
+  event.preventDefault() // modifico la acción por defecto de un formulario
+  let welcome = document.querySelector("#bienvenida")
+  let name = document.querySelector("#nombre").value
+  let date = document.querySelector("#fecha").value
+  const year = new Date().getFullYear()
+  let years
+  let message
+
+  if ([name, date].includes("")) {
+    message = "Todos los campos son obligatorios"
+    welcome.textContent = message
+
+    setTimeout(() => {
+      welcome.textContent = ""
+    }, 1500);
+
+    return // corto la ejcución del código
+  }
+
+  if (date) {
+    // JavaScript intenta convertir 'date' a número automáticamente durante la resta
+    years = year - Number(date)
+  }
+  
+  message = `Bienvenido ${name}! Tienes ${years} años`
+  welcome.textContent = message
+
+  setTimeout(() => {
+    welcome.textContent = ""
+    name = ""
+    date = ""
+  }, 1000);
 }
+
+const myForm = document.querySelector("#formulario")
+myForm.addEventListener("submit", recieveData)
